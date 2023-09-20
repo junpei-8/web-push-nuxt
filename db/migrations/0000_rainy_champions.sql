f2c8b3fe012378af6b3beded4c1b5f02cd0bf1d0
junpei-8 <junpei.m.x08@gmail.com>
2023-09-20 19:21:23 +0900

    update: マイグレーションファイルを変更

--- db/migrations/0000_rainy_champions.sql
+++ db/migrations/0000_rainy_champions.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `web_push_registrations` (
 	`id` serial AUTO_INCREMENT NOT NULL,
-	`endpoint` text NOT NULL,
+	`endpoint` varchar(8000) NOT NULL,
 	`auth_key` text NOT NULL,
 	`p256dh_key` text NOT NULL,
 	`created_at` timestamp NOT NULL DEFAULT (now()),
 	CONSTRAINT `web_push_registrations_id` PRIMARY KEY(`id`),
-	CONSTRAINT `ui_web_push_registrations_endpoint` UNIQUE(`endpoint`)
+	CONSTRAINT `web_push_registrations_endpoint_unique` UNIQUE(`endpoint`)
 );
--- db/schema/web_push_registrations.ts
+++ db/schema/web_push_registrations.ts
@@ -4,20 +4,15 @@ import {
   text,
   timestamp,
   uniqueIndex,
+  varchar,
 } from 'drizzle-orm/mysql-core'
 
 const tableName = 'web_push_registrations'
 
-export default mysqlTable(
-  tableName,
-  {
-    id: serial('id').primaryKey(),
-    endpoint: text('endpoint').notNull(),
-    authKey: text('auth_key').notNull(),
-    p256dhKey: text('p256dh_key').notNull(),
-    createdAt: timestamp('created_at').defaultNow().notNull(),
-  },
-  (table) => ({
-    endpointIndex: uniqueIndex(`ui_${tableName}_endpoint`).on(table.endpoint),
-  })
-)
+export default mysqlTable(tableName, {
+  id: serial('id').primaryKey(),
+  endpoint: varchar('endpoint', { length: 8000 }).notNull().unique(),
+  authKey: text('auth_key').notNull(),
+  p256dhKey: text('p256dh_key').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+})
