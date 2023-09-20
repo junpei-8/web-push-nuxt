5b49717e2b15cfbd2b2d6ed794c657157293133d
junpei-8 <junpei.m.x08@gmail.com>
2023-09-20 19:25:49 +0900

    update: endpoint のバイトを修正

--- db/migrations/0000_colorful_morg.sql
+++ db/migrations/0000_colorful_morg.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `web_push_registrations` (
 	`id` serial AUTO_INCREMENT NOT NULL,
-	`endpoint` varchar(3072) NOT NULL,
+	`endpoint` varchar(768) NOT NULL,
 	`auth_key` text NOT NULL,
 	`p256dh_key` text NOT NULL,
 	`created_at` timestamp NOT NULL DEFAULT (now()),
--- db/schema/web_push_registrations.ts
+++ db/schema/web_push_registrations.ts
@@ -10,7 +10,7 @@ const tableName = 'web_push_registrations'
 
 export default mysqlTable(tableName, {
   id: serial('id').primaryKey(),
-  endpoint: varchar('endpoint', { length: 3072 }).notNull().unique(),
+  endpoint: varchar('endpoint', { length: 768 }).notNull().unique(),
   authKey: text('auth_key').notNull(),
   p256dhKey: text('p256dh_key').notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
