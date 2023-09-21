9876e3fd4b9b5149e6fc833c6c8eef82ba200296
junpei-8 <junpei.m.x08@gmail.com>
2023-09-21 16:23:01 +0900

    update: マイグレーションファイルを修正

---
+++ database/migrations/0001_flat_owl.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `web_push_subscriptions` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`endpoint` varchar(768) NOT NULL,
+	`auth_key` text NOT NULL,
+	`p256dh_key` text NOT NULL,
+	`expired_at` timestamp,
+	`created_at` timestamp NOT NULL DEFAULT (now()),
+	CONSTRAINT `web_push_subscriptions_id` PRIMARY KEY(`id`),
+	CONSTRAINT `web_push_subscriptions_endpoint_unique` UNIQUE(`endpoint`)
+);
+--> statement-breakpoint
+DROP TABLE `web_push_registrations`;
