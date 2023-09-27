d79877f689e917b456a1f95d177d7211d6df10a6
junpei-8 <junpei.v8@gmail.com>
2023-09-27 16:07:21 +0900

    update: sw の実装

--- sw/scripts/web-push.ts
+++ sw/scripts/web-push.ts
@@ -32,32 +32,34 @@ sw.addEventListener('notificationclick', function (event) {
   const clients = sw.clients
 
   event.waitUntil(
-    clients.matchAll({ type: 'window' }).then(function (matchedClients) {
-      const noticeData: NotificationData = event.notification.data || {}
+    clients
+      .matchAll({ type: 'window', includeUncontrolled: true })
+      .then(function (matchedClients) {
+        const noticeData: NotificationData = event.notification.data || {}
 
-      const originUrl = sw.location.origin
-      const url = originUrl + noticeData.url || ''
+        const originUrl = sw.location.origin
+        const url = originUrl + noticeData.url || ''
 
-      console.log('matchedClients', matchedClients)
-      setTimeout(() => {
         console.log('matchedClients', matchedClients)
-        console.log('url: ', url)
-        console.log('originUrl: ', originUrl)
-      }, 2000)
+        setTimeout(() => {
+          console.log('matchedClients', matchedClients)
+          console.log('url: ', url)
+          console.log('originUrl: ', originUrl)
+        }, 2000)
 
-      // 既に開いているタブがあれば、そちらをフォーカスして、URLを更新する
-      for (let i = 0; i < matchedClients.length; i++) {
-        const client = matchedClients[i]
-        setTimeout(() => console.log('client: : ', client), 2000)
-        if (client.url === url) {
-          targetClientUrl = client.url
-          return client.focus ? client.focus() : Promise.resolve(null)
+        // 既に開いているタブがあれば、そちらをフォーカスして、URLを更新する
+        for (let i = 0; i < matchedClients.length; i++) {
+          const client = matchedClients[i]
+          setTimeout(() => console.log('client: : ', client), 2000)
+          if (client.url === url) {
+            targetClientUrl = client.url
+            return client.focus ? client.focus() : Promise.resolve(null)
+          }
         }
-      }
 
-      setTimeout(() => console.log('openWindow: ', targetClientUrl), 2000)
-      return clients.openWindow(url)
-    })
+        setTimeout(() => console.log('openWindow: ', targetClientUrl), 2000)
+        return clients.openWindow(url)
+      })
   )
 })
 
